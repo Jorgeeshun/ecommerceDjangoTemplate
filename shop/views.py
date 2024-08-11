2287835adc156a5e896abec9df39d2f4686c99a8
Jorgeeshun <georgeprinceakeshun23@gmail.com>
2024-08-11 19:25:30 +0000

    Changes to be committed: 	modified:   shop/__pycache__/views.cpython-312.pyc 	modified:   shop/views.py

--- shop/views.py
+++ shop/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Welcome to my book store.")
+    return HttpResponse("Welcome to my shop store.")
